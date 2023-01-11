bd8f8e933c68ff35d89d49ef0d39b90cb9f33da9
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-11 09:47:23 +0000

    first working version

--- Program.cs
+++ Program.cs
@@ -1,89 +1,116 @@
 using System;
-// using System.Text;
-using System.Collections.Generic;
 using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
 using Kiota.Builder;
 using Kiota.Builder.Configuration;
 using Microsoft.Extensions.Logging;
-// using System.IO.Compression;
-// using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Versioning;
 
 Console.WriteLine("Hello browser!");
 
+[SupportedOSPlatform("browser")]
 public partial class KiotaJs
 {
+    private static readonly ThreadLocal<HashAlgorithm> HashAlgorithm = new(() => SHA256.Create());
+    private static string DescriptionUrl = "https://raw.githubusercontent.com/microsoft/kiota/main/tests/Kiota.Builder.IntegrationTests/ToDoApi.yaml";
+
     [JSExport]
-    internal static Task<string> Generate(string name)
+    internal async static Task<string> Generate(string name)
     {
         Console.WriteLine("Starting to Generate " + name);
-        var generationConfiguration = new GenerationConfiguration{
-            OpenAPIFilePath = "https://raw.githubusercontent.com/swagger-api/swagger-petstore/master/src/main/resources/openapi.yaml",
-            IncludePatterns = new HashSet<string>(),
-            ExcludePatterns = new HashSet<string>(),
-            Language = GenerationLanguage.Java,
-            OutputPath = "./test.zip",
+
+        var defaultConfiguration = new GenerationConfiguration();
+
+        var hashedUrl = BitConverter.ToString(HashAlgorithm.Value!.ComputeHash(Encoding.UTF8.GetBytes(DescriptionUrl))).Replace("-", string.Empty);
+        string OutputPath = Path.Combine(Path.GetTempPath(), "kiota", "generation", hashedUrl);
+
+        if (File.Exists(OutputPath))
+        {
+            Console.WriteLine("Deleting OutputPath");
+            File.Delete(OutputPath);
+        }
+        Directory.CreateDirectory(OutputPath);
+
+        var generationConfiguration = new GenerationConfiguration
+        {
+            OpenAPIFilePath = DescriptionUrl,
+            IncludePatterns = defaultConfiguration.IncludePatterns,
+            ExcludePatterns = defaultConfiguration.ExcludePatterns,
+            Language = GenerationLanguage.CSharp,
+            OutputPath = OutputPath,
             ClientClassName = "ApiClient",
             ClientNamespaceName = "io.dummy",
             IncludeAdditionalData = false,
             UsesBackingStore = false,
-            Serializers = new HashSet<string>(),
-            Deserializers = new HashSet<string>(),
-            StructuredMimeTypes = new HashSet<string>(),
+            Serializers = defaultConfiguration.Serializers,
+            Deserializers = defaultConfiguration.Deserializers,
+            StructuredMimeTypes = defaultConfiguration.StructuredMimeTypes,
+            CleanOutput = true,
+            ClearCache = true,
         };
 
         var consoleLogger = new ConsoleLogger();
-        var builder = new KiotaBuilder(consoleLogger, generationConfiguration, new System.Net.Http.HttpClient());
 
-        // bool result = builder.GenerateClientAsync(new()).Result;
+        var builder = new KiotaBuilder(consoleLogger, generationConfiguration, new HttpClient());
 
-        return builder.GenerateClientAsync(new()).ContinueWith(res => "Hello");
-        // bool result = Task.Run(() => builder.GenerateClientAsync(new())).Result;
-        // try {
-        //     bool result = Task.Run(() => builder.GenerateClientAsync(new())).Result;
-        //     var zipFilePath = Path.Combine(Path.GetTempPath(), "kiota", "clients", hashedUrl, "client.zip");
-        //     if (File.Exists(zipFilePath))
-        //         File.Delete(zipFilePath);
-        //     else
-        //         Directory.CreateDirectory(Path.GetDirectoryName(zipFilePath)!);
-        //     ZipFile.CreateFromDirectory(OutputPath, zipFilePath);
+        CancellationTokenSource source = new CancellationTokenSource();
+        CancellationToken token = source.Token;
 
-        //     Console.WriteLine("DEBUG!");
-        //     Console.WriteLine(logBuilder.ToString());
+        try
+        {
+            var result = await builder.GenerateClientAsync(token).ConfigureAwait(false);
+            // it's failing to write the final lock file ...
+        }
+        catch (Exception ex)
+        {
+            // TODO: it's failing to write the lock file need more investigation!
+            Console.WriteLine(ex.StackTrace);
+        }
 
-        //     Console.WriteLine("ZipFile: " + zipFilePath);
-        //     return zipFilePath;
-        // } catch (Exception ex) {
-        //     Console.WriteLine("Error " + ex);
-        //     throw ex;
-        // }
-        // return "Hello world, " + name + " result is " + result;
+        var zipFilePath = Path.Combine(Path.GetTempPath(), "kiota", "clients", hashedUrl, "client.zip");
+        if (File.Exists(zipFilePath))
+            File.Delete(zipFilePath);
+        else
+            Directory.CreateDirectory(Path.GetDirectoryName(zipFilePath)!);
+
+        ZipFile.CreateFromDirectory(OutputPath, zipFilePath);
+
+        // Return Base64???
+        byte[] fileBytes = File.ReadAllBytes(zipFilePath);
+        string base64Content = System.Convert.ToBase64String(fileBytes);
+        return base64Content;
     }
 }
 
 class ConsoleLogger : ILogger<KiotaBuilder>
 {
-  IDisposable? ILogger.BeginScope<TState>(TState state)
-  {
-    return new DummyDisposable();
-  }
+    IDisposable? ILogger.BeginScope<TState>(TState state)
+    {
+        return new DummyDisposable();
+    }
 
-  bool ILogger.IsEnabled(LogLevel logLevel)
-  {
-    return true;
-  }
+    bool ILogger.IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
 
-  void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-  {
-    // dumb implementation
-    Console.WriteLine(formatter(state, exception));
-  }
+    void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        // dumb implementation
+        Console.WriteLine(formatter(state, exception));
+    }
 }
 
 class DummyDisposable : IDisposable
 {
-  public void Dispose()
-  {
-    // Do nothing
-  }
+    public void Dispose()
+    {
+        // Do nothing
+    }
 }
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 import { dotnet } from './dotnet.js'
 
 const { getAssemblyExports, getConfig } = await dotnet
-    .withDiagnosticTracing(true)
+    .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
 
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
 
-const msg = exports.KiotaJs.Generate("test");
-console.log(msg);
+// const msg = await exports.KiotaJs.Generate("test");
+// console.log(msg);
+const data = await exports.KiotaJs.Generate("test");
+
+var element = document.createElement('a');
+// Base64 approach from: https://stackoverflow.com/a/51759464
+element.setAttribute('href', 'data:text/plain;base64,' + data);
+element.download = 'kiota-test.zip';
+element.innerText = 'Download';
+
+document.body.appendChild(element);
